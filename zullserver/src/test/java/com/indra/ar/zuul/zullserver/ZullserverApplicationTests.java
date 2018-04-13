c99fa2228b241922fdb39c801ab18c1ed4d9bb8d
aescobart <aescobart@omeras10.indra.es>
2018-04-13 08:36:54 +0200

    Zull Server

---
+++ zullserver/src/test/java/com/indra/ar/zuul/zullserver/ZullserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.indra.ar.zuul.zullserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZullserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
